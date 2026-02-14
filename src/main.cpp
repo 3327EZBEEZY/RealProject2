69457b0d487f4bde24c31acdb05f20f601466f77
3327E <airports.planes@gmail.com>
2026-02-13 22:55:22 -0500

    2/13 commit 2

--- src/main.cpp
+++ src/main.cpp
@@ -278,6 +278,8 @@ void autonomous() {
  */
 void opcontrol() {
     int snapCounter = 0; // Counter to track how many times we've snapped the position
+    double snapPosx;
+    double snapPosy;
     // pneumatics controller
     pros::Task pneumatics_task_handle(pneumatics_task);
     // loop to continuously update ALL motors
@@ -318,8 +320,8 @@ void opcontrol() {
            motorLastFlywheel.move(0); // Just don't spin this one
         }
         else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)){ //Descore button
-           double snapPosx = chassis.getPose().x;
-           double snapPosy = chassis.getPose().y;
+           snapPosx = chassis.getPose().x;
+           snapPosy = chassis.getPose().y;
            pros::lcd::print(3+snapCounter,"X: %f", snapPosx); // x
            pros::lcd::print(4+snapCounter,"Y: %f", snapPosy); // y 
            pros::delay(2500); // Wait 2.5 second before incrementing snapCounter to prevent doubleortripple clicks
